8eb69d1117840147ff7f327a290127f77d3ec13f
Stephen Danton <stephen.danton@gmail.com>
2019-04-12 14:16:42 -0700

    Adds better selection rectangle highlight colouring

--- LabelIconMatcher.cs
+++ LabelIconMatcher.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEditor;
 using System.Text;
 
@@ -6,9 +6,13 @@ using System.Text;
 public class LabelIconMatcher
 {
     private static Vector2 offset = new Vector2(20, 2);
-    private static Vector2 iconOffset = new Vector2 (3, 2);
-    private static Vector2 padding = new Vector2 (50, 0);
-    
+    private static Vector2 iconOffset = new Vector2(3, 2);
+    private static Vector2 padding = new Vector2(50, 0);
+
+    private static Color UI_base = GLB.ColorFromHex("383838");
+    private static Color UI_hover = GLB.ColorFromHex("303030");
+    private static Color UI_selected = GLB.ColorFromHex("3E5F96");
+
     static LabelIconMatcher()
     {
         EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
@@ -26,50 +30,101 @@ public class LabelIconMatcher
         return ((float)int.Parse(str, System.Globalization.NumberStyles.HexNumber) / 255f);
     }
 
-    static Color getColourForTooltip(string label)
+    static Color getColourForTooltip(string label, bool selected = false)
     {
         Color ret = Color.white;
 
-        switch (label)
+        if (selected == false)
         {
-            // gray
-            case "sv_label_0":
-                ret = ColorFromHex ("A58E7A");
+            switch (label)
+            {
+                // gray
+                case "sv_label_0":
+                    ret = ColorFromHex("A58E7A");
+                    break;
+
+                // blue
+                case "sv_label_1":
+                    ret = ColorFromHex("609BE2");
+                    break;
+
+                // teal
+                case "sv_label_2":
+                    ret = ColorFromHex("50D6A5");
+                    break;
+
+                // green
+                case "sv_label_3":
+                    ret = ColorFromHex("4FE247");
+                    break;
+
+                // yellow
+                case "sv_label_4":
+                    ret = ColorFromHex("F4E33E");
+                    break;
+
+                // orange
+                case "sv_label_5":
+                    ret = ColorFromHex("F28840");
+                    break;
+
+                // red
+                case "sv_label_6":
+                    ret = ColorFromHex("DD4A4A");
+                    break;
+
+                // purple
+                case "sv_label_7":
+                    ret = ColorFromHex("B066D3");
+                    break;
+            }
+        }
+
+        return ret;
+    }
+
+    private static int _hoveredInstance;
+
+    static Color getBackgroundColorForMouseMove(int instanceID, Rect selectionRect)
+    {
+        Color ret = UI_base;
+
+        if (instanceID == _hoveredInstance)
+        {
+            ret = UI_hover;
+        }
+
+        var current = Event.current;
+
+        switch (current.type)
+        {
+            case EventType.MouseDown:
+
+                if (selectionRect.Contains(current.mousePosition))
+                {
+                    ret = UI_selected;
+                }
+
                 break;
 
-            // blue
-            case "sv_label_1":
-                ret = ColorFromHex("609BE2");
-                break;
+            case EventType.Layout:
 
-            // teal
-            case "sv_label_2":
-                ret = ColorFromHex("50D6A5");
-                break;
+                if (selectionRect.Contains(current.mousePosition))
+                {
+                    if (_hoveredInstance != instanceID)
+                    {
+                        _hoveredInstance = instanceID;
+                        ret = UI_hover;
+                    }
+                }
+                else
+                {
+                    if (_hoveredInstance == instanceID)
+                    {
+                        _hoveredInstance = 0;
+                    }
+                }
 
-            // green
-            case "sv_label_3":
-                ret = ColorFromHex("4FE247");
-                break;
-
-            // yellow
-            case "sv_label_4":
-                ret = ColorFromHex("F4E33E");
-                break;
-
-            // orange
-            case "sv_label_5":
-                ret = ColorFromHex("F28840");
-                break;
-
-            // red
-            case "sv_label_6":
-                ret = ColorFromHex("DD4A4A");
-                break;
-
-            // purple
-            case "sv_label_7":
-                ret = ColorFromHex("B066D3");
                 break;
         }
 
@@ -81,20 +136,41 @@ public class LabelIconMatcher
         GameObject gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
 
         Color fontColor = Color.red;
+        Color iconColor = Color.white;
         FontStyle style = FontStyle.Normal;
-        Color backgroundColor = new Color(0.22f, 0.22f, 0.22f); // dark theme colour
+        Color backgroundColor = UI_base;
+        bool selected = false;
 
         var obj = EditorUtility.InstanceIDToObject(instanceID);
         if (obj != null)
         {
+            for (int i = 0; i < UnityEditor.Selection.gameObjects.Length; i++)
+            {
+                if (UnityEditor.Selection.gameObjects[i] == gameObject)
+                {
+                    backgroundColor = UI_selected;
+                    selected = true;
+                    break;
+                }
+            }
+
+            if (selected == false)
+            {
+                backgroundColor = getBackgroundColorForMouseMove(instanceID, selectionRect);
+            }
+
             var prefabType = PrefabUtility.GetPrefabType(obj);
             if (prefabType == PrefabType.PrefabInstance)
-            {                
-                GUIContent content = EditorGUIUtility.ObjectContent (obj, null);
-                fontColor = getColourForTooltip (content.image.name);                
+            {
+                // Texture2D tex = PrefabUtility.GetIconForGameObject (gameObject);
+
+                GUIContent content = EditorGUIUtility.ObjectContent(obj, null);
+
+                iconColor = getColourForTooltip(content.image.name, false);
+                fontColor = getColourForTooltip(content.image.name, selected);                
 
                 Rect offsetRect = new Rect(selectionRect.position + offset, selectionRect.size + padding);
-                EditorGUI.DrawRect(selectionRect, backgroundColor);                
+                EditorGUI.DrawRect(selectionRect, backgroundColor);
 
                 EditorGUI.LabelField(offsetRect, obj.name, new GUIStyle()
                 {
@@ -104,7 +180,7 @@ public class LabelIconMatcher
                 );
 
                 Rect iconRect = new Rect(selectionRect.position + iconOffset, new Vector2(12, 12));
-                EditorGUI.DrawRect(iconRect, fontColor);
+                EditorGUI.DrawRect(iconRect, iconColor);
             }
         }
     }
